1f03ba4f28f8eccb14edf0ef4e120f6b48a39c53
Laithonex <laith.aljufaili966@gmail.com>
2020-03-26 09:59:50 +0400

    Frindship backend is ready

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_044142) do
+ActiveRecord::Schema.define(version: 2020_03_26_054315) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "firend_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["firend_id"], name: "index_friendships_on_firend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -43,6 +52,8 @@ ActiveRecord::Schema.define(version: 2020_03_26_044142) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "firend_id"
   add_foreign_key "user_stocks", "stocks"
   add_foreign_key "user_stocks", "users"
 end
