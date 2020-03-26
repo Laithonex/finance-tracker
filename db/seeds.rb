21d8215ffb2162a4e5cb97e65d61c5199fcc33b2
Laithonex <laith.aljufaili966@gmail.com>
2020-03-26 09:41:50 +0400

    Complete Signup assgiment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Stock.destroy(ticker: 'AAPL', name: 'Apple Inc.')
+# Stock.destroy(ticker: 'AMZN', name: 'Amazon Inc.')
+# Stock.create(ticker: 'FB', name: 'Facebook Inc.')
+# User.create(email: 'user1@user.com', password: 'user1test')
+# User.create(email: 'user2@user.com', password: 'user2test')
+# User.create(email: 'user3@user.com', password: 'user3test')
